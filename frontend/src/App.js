2474de319c63c7b33bb16288468842e207a27bf2
nikhiluppala <uppala.n@northeastern.edu>
2024-02-02 17:53:39 -0500

    Worked on Shoping page

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>}></Route>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}></Route>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}></Route>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}></Route>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productID' element={<Product />} />
+        </Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/Login' element={<LoginSignup/>}></Route>
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
